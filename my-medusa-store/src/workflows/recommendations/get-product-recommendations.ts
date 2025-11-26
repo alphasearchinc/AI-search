2e6bbd2733483b541e63a64843c8f4f4077330a5
Karl Sixten Bjarnø <karlsixten@gmail.com>
2025-11-26 13:16:40 +0100

    Refactor getProductRecommendationsWorkflow to specify return type as RecommendationsResult

--- my-medusa-store/src/workflows/recommendations/get-product-recommendations.ts
+++ my-medusa-store/src/workflows/recommendations/get-product-recommendations.ts
@@ -1,22 +1,16 @@
 import { createWorkflow, WorkflowResponse } from "@medusajs/workflows-sdk";
 import { getProductEmbeddingStep } from "./steps/get-product-embedding";
 import { getSimilarProductsStep } from "./steps/get-similar-products";
+import { RecommendationsResult } from "../../modules/elasticsearch/types";
 
 type GetProductRecommendationsInput = {
   product_id: string;
   limit: number;
 };
 
-type GetProductRecommendationsOutput = {
-  product_id: string;
-  hits: any[];
-  count: number;
-  searchDuration: number;
-};
-
 export const getProductRecommendationsWorkflow = createWorkflow(
   "get-product-recommendations",
-  (input: GetProductRecommendationsInput) => {
+  (input: GetProductRecommendationsInput): RecommendationsResult => {
     const { embedding } = getProductEmbeddingStep(input.product_id);
 
     const result = getSimilarProductsStep({
