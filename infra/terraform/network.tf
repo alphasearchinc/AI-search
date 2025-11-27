396e9d3e28614ae095978c1d8eacee8ea88ef86f
Aleksandergreg <alek3691@stud.kea.dk>
2025-11-27 09:54:35 +0100

    Added the python embedder in the ansible docker file, allowed network traffic for it

--- infra/terraform/network.tf
+++ infra/terraform/network.tf
@@ -84,6 +84,19 @@ resource "azurerm_network_security_group" "nsg" {
     destination_address_prefix = "*"
   }
 
+  # Python embedder service
+  security_rule {
+    name                       = "PythonEmbedder"
+    priority                   = 1040
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "1337"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
   tags = local.common_tags
 }
 
